c19ef633c813c78f7ad5b21a47a59e78b1f2961f
martinkranec <kranec@netgrif.com>
2024-01-22 13:48:45 +0100

    [ETASK-13] Upgrade engineu na 6.4.0 - update version - migration updates

--- src/main/groovy/com/netgrif/etask/EtaskActionDelegate.groovy
+++ src/main/groovy/com/netgrif/etask/EtaskActionDelegate.groovy
@@ -23,56 +23,6 @@ class EtaskActionDelegate extends ActionDelegate {
     private UriNodeDataRepository uriNodeDataRepository
 
     /**
-     * create or update menu item of specified type
-     * @param id
-     * @param uri
-     * @param type
-     * @param query
-     * @param icon
-     * @param title
-     * @param allowedNets
-     * @param roles
-     * @param bannedRoles
-     * @return
-     */
-    Case createOrUpdateMenuItem(String id, String uri, String type, String query, String icon, String title, List<String> allowedNets, Map<String, String> roles = [:], Map<String, String> bannedRoles = [:]) {
-        collectRolesForPreferenceItem(roles)
-        Case menuItem = findMenuItem(id)
-        if (!menuItem) {
-            Case filter = createFilter(title, query, type, allowedNets, icon, "private", null)
-            createUri(uri, UriContentType.DEFAULT)
-            return createMenuItem(uri, id, filter, roles, bannedRoles)
-        } else {
-            Case filter = getFilterFromMenuItem(menuItem)
-            changeFilter filter query { query }
-            changeFilter filter allowedNets { allowedNets }
-            changeFilter filter title { title }
-            changeFilter filter icon { icon }
-            changeMenuItem menuItem allowedRoles { roles }
-            changeMenuItem menuItem bannedRoles { bannedRoles }
-            changeMenuItem menuItem uri { uri }
-            changeMenuItem menuItem filter { filter }
-            return workflowService.findOne(menuItem.stringId)
-        }
-    }
-
-    private Map<String, I18nString> collectRolesForPreferenceItem(Map<String, String> roles) {
-        Map<String, PetriNet> temp = [:]
-        return roles.collectEntries { entry ->
-            if (!temp.containsKey(entry.value)) {
-                temp.put(entry.value, petriNetService.getNewestVersionByIdentifier(entry.value))
-            }
-            PetriNet net = temp[entry.value]
-            def foundEntry = net.roles.find { it.value.importId == entry.key }
-            if (!foundEntry) {
-                throw new IllegalArgumentException("No role $entry.key $net.identifier")
-            }
-            ProcessRole role = foundEntry.value
-            return [(role.importId + ":" + net.identifier), ("$role.name ($net.title)" as String)]
-        } as Map<String, I18nString>
-    }
-
-    /**
      * update menu item property
      * @param id
      * @param section to put this item in instead of default (currently only "settings" supported)
